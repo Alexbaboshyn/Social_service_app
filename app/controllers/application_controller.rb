8b520018c911fe9d8a20e7f7a31cf6b956ea3365
Alexbaboshyn <Alexbaboshyn@gmail.com>
2017-08-20 15:16:43 +0300

    Some logic in the ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  skip_before_action :verify_authenticity_token
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    @exception = exception
+
+    head 404
+  end
+
+  rescue_from ActiveRecord::RecordInvalid, ActiveModel::StrictValidationFailed do
+    render :errors, status: :unprocessable_entity
+  end
+
+  def create
+    build_resource
+
+    resource.save!
+  end
+
+  def update
+    resource.update! resource_params
+  end
+
+  def destroy
+    resource.destroy!
+  end
 end
